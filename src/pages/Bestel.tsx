1cf052a53a28f4e805fe2b0dac6e1a7636f500f5
Vigintillionn <yarne.ramakers@gmail.com>
2022-11-24 22:09:41 +0100

    Made order page functional

--- src/pages/Bestel.tsx
+++ src/pages/Bestel.tsx
@@ -1,10 +1,19 @@
+import emailjs from "@emailjs/browser";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Dispatch, SetStateAction, useState } from "react";
-import { useSearchParams } from "react-router-dom";
+import { useRef, useState } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { v4 as uuidv4 } from 'uuid';
 import NumericInput from "../components/NumericInput";
-import TextInput from "../components/TextInput";
 
+const PUBLIC_KEY = "VydYsQWvyQEaGrLIJ";
+const SERVICE_ID = "service_5pg4oe3";
+const TEMPLATE_ID_ORDER = "template_5d8ydcy";
+
+// TODO: Change to ecobol emailjs
+const P_KEY = "g9rIdgXMYLKten8iD";
+const S_ID = "service_zz8m5br";
+const TEMPLATE_ID_CONFIRMATION = "template_whgpq6h";
 
 export default function Bestel() {
   const [searchParams] = useSearchParams();
@@ -12,55 +21,111 @@ export default function Bestel() {
 
   const [error, setError] = useState("");
 
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [address, setAddress] = useState("");
   const [postalCode, setPostalCode] = useState<number | undefined>(undefined);
-  const [city, setCity] = useState("");
+
+  const navigate = useNavigate();
+
+  const form = useRef(null);
 
   let bottleType = searchParams.get("type");
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(e.nativeEvent.submitter.id !== "orderbutton") return;
+
+    let firstName = (form.current[3] as React.InputHTMLAttributes<HTMLFormElement>).value.toString();
+    let lastName = (form.current[4] as React.InputHTMLAttributes<HTMLFormElement>).value.toString();
+    let email = (form.current[5] as React.InputHTMLAttributes<HTMLFormElement>).value.toString();
+    let address = (form.current[6] as React.InputHTMLAttributes<HTMLFormElement>).value.toString();
+    let pCode = (form.current[7] as React.InputHTMLAttributes<HTMLFormElement>).value.toString();
+    let city = (form.current[8] as React.InputHTMLAttributes<HTMLFormElement>).value.toString();
+    let comments = (form.current[9] as React.InputHTMLAttributes<HTMLFormElement>).value.toString();
+
+    // TODO: Check for valid
+    let err: string[] = [];
+    if (!firstName || !lastName  || !email || !address || !pCode || !city) err.push("Gelieve alle velden in te vullen.");
+    if (!email.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g)) err.push("Vul alstublieft een correct e-mail adres in.");
+    // TODO: Make it send mail
+    if (err.length > 0) {
+      setError(err.join(" "));
+      return;
+    }
+
+    let orderId = uuidv4().split("-")[0];
+    let price = parseInt(amount)*(bottleType == "wijnfles" ? 12 : 15);
+
+    let params = {
+      firstName,
+      lastName,
+      email,
+      address,
+      pCode,
+      city,
+      comments,
+      amount,
+      bottleType,
+      orderId,
+      price
+    }
+
+    emailjs.send(SERVICE_ID, TEMPLATE_ID_ORDER, params, PUBLIC_KEY)
+    .then((res) => {
+      console.log(res.text);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+
+    emailjs.send(S_ID, TEMPLATE_ID_CONFIRMATION, params, P_KEY)
+    .then((res) => {
+      console.log(res.text);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+
+    navigate(`/betaal?prijs=${price}&email=${email}&fName=${firstName}`);
+  }
+
   return (
-    <div className="relative mb-[40rem] mx-0">
-      <div className="shadow bg-green text-center rounded-md overflow-hidden my-8 h-[36rem] w-[20rem] absolute left-2/4 top-2/4 -translate-x-2/4 md:w-[48rem]">
-        <div className="relative text-center overflow-hidden p-7 h-full w-full float-left md:w-[55%] md:text-left">
+    <div className="relative mb-[50rem] mx-0 md:mb-[44rem]">
+      <div className="shadow bg-green text-center rounded-md overflow-hidden my-8 h-[45rem] w-[20rem] absolute left-2/4 top-2/4 -translate-x-2/4 md:h-[40rem] md:w-[48rem]">
+        <form ref={form} onSubmit={handleSubmit} id="order" className="relative text-center overflow-hidden p-7 h-full w-full float-left md:w-[55%] md:text-left">
           <h1 className="font-mavenpro inline-block relative text-3xl m-0 text-gunmetal uppercase">{bottleType}</h1>
           <div>
-            <div>  
+            <div>
               Aantal:
               <NumericInput className="ml-2" min={1} max={10} val={amount} onValChange={setAmount} />
-              {/* { bottleType === "wijnfles" && 
-                <div>
-                  Grootte:
-                  <Select options={[{ "name": "1L" }, { "name": "10L" }]} values={[{ "name": "1L" }]} onChange={ (val) => console.log(val) }/>
-                </div> 
-              } */}
             </div>
-            <div className="w-full grid md:grid-cols-4 md:grid-rows-5 mt-4 text-gunmetal font-nunito">
-              <h2 className="row-start-1 row-end-2 col-start-1 col-end-5 text-lg font-mavenpro  uppercase font-bold m-auto">Leveringsinformatie</h2>
-              <input type="text" placeholder="Voornaam" className="row-start-2 row-end-2 col-start-1 col-end-3 mr-2 my-1 p-2 rounded-sm" value={""} />
-              <input type="text" placeholder="Naam" className="row-start-2 row-end-2 col-start-3 col-end-5 mr-2 my-1 p-2 rounded-sm" />
-              <input type="email" placeholder="E-mail" className="row-start-3 row-end-3 col-start-1 col-end-5 mr-2 my-1 p-2 rounded-sm" />
-              <input type="text" placeholder="Adres" className="row-start-4 row-end-4 col-start-1 col-end-5 mr-2 my-1 p-2 rounded-sm" />
-              <input type="number" placeholder="Postcode" className="row-start-5 row-end-5 col-start-1 col-end-2 mr-2 my-1 p-2 rounded-sm" value={postalCode ?? ""} onChange={(e) => setPostalCode(isNaN(parseInt(e.target.value)) ? undefined : parseInt(e.target.value))}  />
-              <input type="text" placeholder="Stad" className="row-start-5 row-end-5 col-start-2 col-end-5 mr-2 my-1 p-2 rounded-sm" />
+            <div className="w-full flex flex-col md:grid md:grid-cols-4 md:grid-rows-5 mt-4 text-gunmetal font-nunito">
+              <h2 className="row-start-1 row-end-2 col-start-1 col-end-5 text-lg font-mavenpro  uppercase font-bold m-auto">Leveringsinformatie<span className="text-red font-normal text-2xl">*</span></h2>
+              <input type="text" placeholder="Voornaam" className="ml-2 mr-2 my-1 p-2 rounded-sm md:row-start-2 md:row-end-2 md:col-start-1 md:col-end-3 md:ml-0" />
+              <input type="text" placeholder="Naam" className="ml-2 mr-2 my-1 p-2 rounded-sm md:row-start-2 md:row-end-2 md:col-start-3 md:col-end-5 md:ml-0" />
+              <input type="email" placeholder="E-mail" className="ml-2 mr-2 my-1 p-2 rounded-sm md:row-start-3 md:row-end-3 md:col-start-1 md:col-end-5 md:ml-0" />
+              <input type="text" placeholder="Adres" className="ml-2 mr-2 my-1 p-2 rounded-sm md:row-start-4 md:row-end-4 md:col-start-1 md:col-end-5 md:ml-0" />
+              <input type="number" placeholder="Postcode" className="ml-2 mr-2 my-1 p-2 rounded-sm md:row-start-5 md:row-end-5 md:col-start-1 md:col-end-2 md:ml-0" value={postalCode ?? ""} onChange={(e) => setPostalCode(isNaN(parseInt(e.target.value)) ? undefined : parseInt(e.target.value))}  />
+              <input type="text" placeholder="Stad" className="ml-2 mr-2 my-1 p-2 rounded-sm md:row-start-5 md:row-end-5 md:col-start-2 md:col-end-5 md:ml-0" />
             </div>
-            { error.length > 0 && 
-              <span className="text-red block font-nunito">Gelieve en correct e-mail adres in te geven.</span>
+            { error.length > 0 &&
+              <span className="text-red block font-nunito">Gelieve en correct e-mail adres in te geven. Gelieve alle velden in te vullen. </span>
             }
             { (postalCode !== 2580 && postalCode !== 3140) && postalCode?.toString().length === 4 &&
               <span className="text-red block mt-2 font-nunito">Wij leveren niet buiten Putte of Keerbergen, u zult gebruik moeten maken van een afhaalpunt.</span>
             }
+            <div className="w-full text-gunmetal font-nunito text-center">
+              <h2 className="text-lg font-mavenpro  uppercase font-bold my-2">Opmerkingen</h2>
+              <textarea placeholder="Hebt u enige opmerkingen of dingen die wij moeten weten, laat ze hier acher." className="w-full max-h-20 mr-2 my-1 p-2 rounded-sm"></textarea>
+            </div>
           </div>
-          <div className="absolute bottom-[5%] left-1/4 md:left-[8%]">
-            <button type="submit" form="amount" className="bg-blue transition delay-300 ease-linear rounded-md relative overflow-hidden pointer outline-none border-none text-white p-0 m-0 group hover:bg-blue-darker">
-              <span className="font-nunito transition-transform delay-300 inline-block px-3 py-5 m-0 text-lg bg-gunmetal border-0 -translate-x-[10%] pr-4 group-hover:-translate-x-[110%]">€{(parseInt(amount)*12)},-</span>
+          <div className="absolute bottom-[4%] left-1/4 md:left-[6%]">
+            <button type="submit" form="order" id="orderbutton" className="bg-blue transition delay-300 ease-linear rounded-md relative overflow-hidden pointer outline-none border-none text-white p-0 m-0 group hover:bg-blue-darker">
+              <span className="font-nunito transition-transform delay-300 inline-block px-3 py-5 m-0 text-lg bg-gunmetal border-0 -translate-x-[10%] pr-4 group-hover:-translate-x-[110%]">€{(parseInt(amount)*(bottleType == "wijnfles" ? 12 : 15))},-</span>
               <span className="font-nunito transition-transform delay-300 inline-block px-3 py-5 m-0 text-lg bg-gunmetal border-0 -translate-x-full absolute z-1 left-0 top-0 group-hover:translate-x-0"><FontAwesomeIcon icon={faShoppingCart} /></span>
               <span className="font-nunito transition-transform delay-300 inline-block px-3 py-5 m-0 text-lg z-10 font-semibold">Bestel Nu</span>
             </button>
           </div>
-        </div>
+        </form>
+
         <div className="transition-all delay-300 ease-in-out inline-block relative overflow-hidden h-full float-right w-[45%] group max-md:hidden">
           <img className="w-full h-full object-cover transition-transform ease-in-out delay-75 group-hover:scale-105" src={"/Images/" + bottleType +  ".jpg"} alt="" />
           <div className="bg-gunmetal-accent font-nunito transition-all delay-300 ease-in-out -translate-x-full absolute leading-10 text-left text-xl cursor-no-drop text-white h-full w-full left-0 top-0 group-hover:translate-x-0">
